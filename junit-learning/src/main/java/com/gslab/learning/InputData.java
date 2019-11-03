5e0c0c77829e2a236fd1f5a9f54ac15cdf89eadd
KarekarKartik <kartik.karekar@stagrp.com>
2019-11-03 20:05:53 +0530

    code coverage and test cases added.

--- junit-learning/src/main/java/com/gslab/learning/InputData.java
+++ junit-learning/src/main/java/com/gslab/learning/InputData.java
@@ -11,6 +11,28 @@ import org.apache.commons.lang3.StringUtils;
  */
 public class InputData {
 	
+	public enum MONTHS {
+							JAN("JAN",31),FEB("FEB",28),MAR("MAR",31),APR("APR",30),
+							MAY("MAY",31),JUN("JUN",30),JUL("JUL",31),AUG("AUG",31),
+							SEP("SEP",30),OCT("OCT",31),NOV("NOV",30),DEC("DEC",31);
+							
+							private String month;
+							private int noOfDays;
+	
+							MONTHS(String month,int noOfDays) {
+								this.month= month;
+								this.noOfDays= noOfDays;
+							}
+
+							public String getMonth() {
+								return month;
+							}
+
+							public int getNoOfDays() {
+								return noOfDays;
+							}
+						};
+	
 	public int addition(int number1, int number2) {
 		return number1 + number2;
 	}
@@ -18,5 +40,40 @@ public class InputData {
 	public boolean isPalindrome(String inputString) {
 		StringBuilder tempStringLocation = new StringBuilder(inputString);
 		return (StringUtils.equalsIgnoreCase(inputString, tempStringLocation.reverse().toString())) ? true : false;
+	}
+	
+	public boolean isPrime(int number) {
+		int index = 2;
+		while(index <= number/2) {
+			if(number%index == 0) {
+				return false;
+			}
+			index++;
+		}
+		return true;
+	}
+	
+	public Double salaryCalculator(double perHourSalary,int numberOfDailyWorkingHours,String month) {
+		if(StringUtils.isNotBlank(month)) {
+			try {
+				if(numberOfDailyWorkingHours >= 5 && numberOfDailyWorkingHours <=  24) {
+					MONTHS monthEnumValue = MONTHS.valueOf(month);
+					int monthlyHours = ((monthEnumValue.getNoOfDays()) * numberOfDailyWorkingHours);
+					if(perHourSalary >= 0) {
+						return perHourSalary * monthlyHours;
+					} else {
+						System.err.println("Invalid Salary perHours."+perHourSalary);
+					}
+				} else {
+					System.err.println("Invalid numberOfDailyWorkingHours."+numberOfDailyWorkingHours);
+				}
+			} catch (IllegalArgumentException e) {
+				System.err.println("Invalid month value passed."+month);
+				throw e;
+			}
+		} else {
+			System.err.println("Invalid month value either empty/null");
+		}
+		return (double) 0;
 	}
 }
--- junit-learning/src/test/java/com/gslab/learning/InputDataTest.java
+++ junit-learning/src/test/java/com/gslab/learning/InputDataTest.java
@@ -1,36 +1,112 @@
 package com.gslab.learning;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertSame;
 import static org.junit.Assert.assertTrue;
 
 import org.junit.Before;
 import org.junit.Test;
+import org.junit.experimental.categories.Categories.ExcludeCategory;
 
 public class InputDataTest {
 	public InputData input;
+	public static int testcaseIndex = 0;
 	
 	@Before
 	public void initalization() {
-		System.out.println("inside initalizer for testcases :");
+//		System.out.println("** inside initalizer for testcases **" + (InputDataTest.testcaseIndex + 1));
 		this.input = new InputData();
+//		InputDataTest.testcaseIndex++;
 	}		
 	
 	@Test
 	public void testShouldTestAddition() {
 		int inputNumber1=10, inputNumber2= 20;
-		int result = this.input.addition(inputNumber1, inputNumber2);
-		
-		assertEquals(3, result);
-		
+		int result = this.input.addition(inputNumber1, inputNumber2);		
+		assertEquals(30, result);
 	}
 	
 	@Test
 	public void testShouldCheckIsStringPolindrome() {
 		String inputString = "ABCDCBA";
-//		assertTrue(this.input.isPalindrome(inputString));
-		
-		assertSame(inputString, this.input.isPalindrome(inputString));
-		
+		assertTrue(this.input.isPalindrome(inputString));
 	}
+	
+	@Test
+	public void testShouldCheckReturnFalseForIsStringPolindrome() {
+		String inputString = "GSlab";
+		assertFalse(this.input.isPalindrome(inputString));
+	}
+	
+	@Test
+	public void testShouldCheckIsPrime() {
+		int number = 7;
+		assertTrue(this.input.isPrime(number));
+	}	
+	
+	@Test
+	public void testShouldReturnFalseForIsPrime() {
+		int number = 20;
+		assertFalse(this.input.isPrime(number));
+	}	
+	
+	@Test
+	public void testShouldCalculateSalary() {
+		double perHourSalary= 100.50;
+		int numberOfDailyWorkingHours= 9;
+		String month= "OCT";
+		double salary = this.input.salaryCalculator(perHourSalary, numberOfDailyWorkingHours, month);
+		assertEquals(28039.5, salary, 0.0);
+	}
+	
+	@Test
+	public void testShouldCalculateSalaryForMonth() {
+		double perHourSalary= 100.50;
+		int numberOfDailyWorkingHours= 9;
+		String month= null;
+		double salary = this.input.salaryCalculator(perHourSalary, numberOfDailyWorkingHours, month);
+		assertEquals(0.0, salary, 0.0);
+	}
+	
+	@Test(expected = IllegalArgumentException.class)
+	public void testShouldThrowIllegalArgumentExceptionForCalculateSalary() {
+		double perHourSalary= 100.50;
+		int numberOfDailyWorkingHours= 9;
+		String month= "JAN1@#@";
+		this.input.salaryCalculator(perHourSalary, numberOfDailyWorkingHours, month);
+	}
+	
+	@Test
+	public void testShouldCalculateSalaryForInvalidNDWH() {
+		double perHourSalary= 100.50;
+		int numberOfDailyWorkingHours= 3;
+		String month= "OCT";
+		double salary = this.input.salaryCalculator(perHourSalary, numberOfDailyWorkingHours, month);
+		assertEquals(0.0, salary, 0.0);
+	}
+	
+	@Test
+	public void testShouldCalculateSalaryForInvalidNDWH2() {
+		double perHourSalary= 100.50;
+		int numberOfDailyWorkingHours= 50;
+		String month= "OCT";
+		double salary = this.input.salaryCalculator(perHourSalary, numberOfDailyWorkingHours, month);
+		assertEquals(0.0, salary, 0.0);
+	}	
+	
+	@Test
+	public void testShouldCalculateSalaryForInvalidPHS() {
+		double perHourSalary= -50;
+		int numberOfDailyWorkingHours= 9;
+		String month= "OCT";
+		double salary = this.input.salaryCalculator(perHourSalary, numberOfDailyWorkingHours, month);
+		assertEquals(0.0, salary, 0.0);
+	}	
+	
+	
+	
+	
+	
+	
 }
